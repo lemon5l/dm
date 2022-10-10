cf41f658f35fe0645f4b83bd005f2554f4af1016
TheEndSouth <112705253+TheEndSouth@users.noreply.github.com>
2022-10-10 19:45:28 +0800

    Update rand.c

--- rand.c
+++ rand.c
@@ -1,24 +1,32 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
  
 int main(){
     int i,j,k;
-    int class1[90][21]={0};
-    int class2[90][21]={0};
-    int class3[90][21]={0};
-    int class4[90][21]={0};
-    int class5[90][21]={0};
+    int class1[90][22]={0};
+    int class2[90][22]={0};
+    int class3[90][22]={0};
+    int class4[90][22]={0};
+    int class5[90][22]={0};
     for(i=1;i<=90;i++){
         class1[i-1][0]=i;
     }
-    srand((unsigned int)time(0));//初始化种子为随机值
+    srand((unsigned int)time(0)*5);//初始化种子为随机值
     j=rand()%4+5;
     i=j;
+    for(j=j-1;j>0;j--){
+        int num = rand()%5;
+        class1[j][21]=num;
+    }
+    j=i;
     for(j=j-1;j>=0;j--){
-        for(int a=1;a<=16;a++){
+        for(int a=1;a<=16;){
             int num = rand()%20+1;
-            class1[j][num]=1;
+            if(class1[j][num]!=1){
+                class1[j][num]=1;
+                a++;
+            }
         }
     }
     for(k=1;k<=20;k++){
@@ -34,10 +42,18 @@ int main(){
     srand((unsigned int)time(0)*10);//初始化种子为随机值
     j=rand()%4+5;
     i=j;
+    for(j=j-1;j>0;j--){
+        int num = rand()%5;
+        class2[j][21]=num;
+    }
+    j=i;
     for(j=j-1;j>=0;j--){
-        for(int a=1;a<=16;a++){
+        for(int a=1;a<=16;){
             int num = rand()%20+1;
-            class2[j][num]=1;
+            if(class2[j][num]!=1){
+                class2[j][num]=1;
+                a++;
+            }
         }
     }
     for(k=1;k<=20;k++){
@@ -53,10 +69,18 @@ int main(){
     srand((unsigned int)time(0)*20);//初始化种子为随机值
     j=rand()%4+5;
     i=j;
+    for(j=j-1;j>0;j--){
+        int num = rand()%5;
+        class3[j][21]=num;
+    }
+    j=i;
     for(j=j-1;j>=0;j--){
-        for(int a=1;a<=16;a++){
+        for(int a=1;a<=16;){
             int num = rand()%20+1;
-            class3[j][num]=1;
+            if(class3[j][num]!=1){
+                class3[j][num]=1;
+                a++;
+            }
         }
     }
     for(k=1;k<=20;k++){
@@ -72,10 +96,18 @@ int main(){
     srand((unsigned int)time(0)*30);//初始化种子为随机值
     j=rand()%4+5;
     i=j;
+    for(j=j-1;j>0;j--){
+        int num = rand()%5;
+        class4[j][21]=num;
+    }
+    j=i;
     for(j=j-1;j>=0;j--){
-        for(int a=1;a<=16;a++){
+        for(int a=1;a<=16;){
             int num = rand()%20+1;
-            class4[j][num]=1;
+            if(class4[j][num]!=1){
+                class4[j][num]=1;
+                a++;
+            }
         }
     }
     for(k=1;k<=20;k++){
@@ -91,10 +123,18 @@ int main(){
     srand((unsigned int)time(0)*40);//初始化种子为随机值
     j=rand()%4+5;
     i=j;
+    for(j=j-1;j>0;j--){
+        int num = rand()%5;
+        class5[j][21]=num;
+    }
+    j=i;
     for(j=j-1;j>=0;j--){
-        for(int a=1;a<=16;a++){
+        for(int a=1;a<=16;){
             int num = rand()%20+1;
-            class5[j][num]=1;
+            if(class5[j][num]!=1){
+                class5[j][num]=1;
+                a++;
+            }
         }
     }
     for(k=1;k<=20;k++){
@@ -105,7 +145,7 @@ int main(){
         }
     }
     for(int a=0;a<90;a++){
-        for(int b=0;b<21;b++){
+        for(int b=0;b<22;b++){
             printf("%d ",class1[a][b]);
             if(b==0){
                 printf("\t");
@@ -115,7 +155,7 @@ int main(){
     }
     printf("\n");
     for(int a=0;a<90;a++){
-        for(int b=0;b<21;b++){
+        for(int b=0;b<22;b++){
             printf("%d ",class2[a][b]);
             if(b==0){
                 printf("\t");
@@ -125,7 +165,7 @@ int main(){
     }
     printf("\n");
     for(int a=0;a<90;a++){
-        for(int b=0;b<21;b++){
+        for(int b=0;b<22;b++){
             printf("%d ",class3[a][b]);
             if(b==0){
                 printf("\t");
@@ -135,7 +175,7 @@ int main(){
     }
     printf("\n");
     for(int a=0;a<90;a++){
-        for(int b=0;b<21;b++){
+        for(int b=0;b<22;b++){
             printf("%d ",class4[a][b]);
             if(b==0){
                 printf("\t");
@@ -145,7 +185,7 @@ int main(){
     }
     printf("\n");
     for(int a=0;a<90;a++){
-        for(int b=0;b<21;b++){
+        for(int b=0;b<22;b++){
             printf("%d ",class5[a][b]);
             if(b==0){
                 printf("\t");
@@ -157,31 +197,31 @@ int main(){
     FILE *fpWrite=fopen("data.txt","w");  
 
     for(int a=0;a<90;a++){
-        for(int b=0;b<21;b++){
+        for(int b=0;b<22;b++){
             fprintf(fpWrite,"%d ",class1[a][b]);
         }
         fprintf(fpWrite,"\n");
     }
     for(int a=0;a<90;a++){
-        for(int b=0;b<21;b++){
+        for(int b=0;b<22;b++){
             fprintf(fpWrite,"%d ",class2[a][b]);
         }
         fprintf(fpWrite,"\n");
     } 
     for(int a=0;a<90;a++){
-        for(int b=0;b<21;b++){
+        for(int b=0;b<22;b++){
             fprintf(fpWrite,"%d ",class3[a][b]);
         }
         fprintf(fpWrite,"\n");
     } 
     for(int a=0;a<90;a++){
-        for(int b=0;b<21;b++){
+        for(int b=0;b<22;b++){
             fprintf(fpWrite,"%d ",class4[a][b]);
         }
         fprintf(fpWrite,"\n");
     } 
     for(int a=0;a<90;a++){
-        for(int b=0;b<21;b++){
+        for(int b=0;b<22;b++){
             fprintf(fpWrite,"%d ",class5[a][b]);
         }
         fprintf(fpWrite,"\n");
